b96097027b315b28765eb08a01e043a93edafc77
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-08-12 16:02:54 +0500

    refactor: removed aside

--- Frontend/src/app/feature/layout/layout.module.ts
+++ Frontend/src/app/feature/layout/layout.module.ts
@@ -1,30 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AsideComponent } from './aside/aside.component';
 import { DefaultLayoutComponent } from './default-layout/default-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { AppMaterialModule } from '../../core/app-material/app-material.module';
 import { FooterComponent } from './footer/footer.component';
 
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    AsideComponent,
-    FooterComponent,
-    DefaultLayoutComponent
-  ],
-  imports: [
-    RouterModule.forChild([]),
-    CommonModule,
-    AppMaterialModule
-  ],
-  exports: [
-    RouterModule,
-    HeaderComponent,
-    AsideComponent,
-    DefaultLayoutComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, DefaultLayoutComponent],
+  imports: [RouterModule.forChild([]), CommonModule, AppMaterialModule],
+  exports: [RouterModule, HeaderComponent, DefaultLayoutComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}
