94d391c83232c960b361f48cc7763856285f498e
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-08-23 15:03:16 +0500

    chore: messaage screen dynamic properties

--- Frontend/src/app/shared/components/message-screen/message-screen.component.ts
+++ Frontend/src/app/shared/components/message-screen/message-screen.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+export interface IMessageSection1 {
+  header: string;
+  paragraphg: string;
+}
 @Component({
   selector: 'app-message-screen',
   templateUrl: './message-screen.component.html',
-  styleUrls: ['./message-screen.component.scss']
+  styleUrls: ['./message-screen.component.scss'],
 })
 export class MessageScreenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input()
+  imgSrc: string;
+  @Input()
+  Section1Data: IMessageSection1;
+  @Input()
+  Section2Data: any;
+  @Input()
+  followUpButton: boolean;
+  @Input()
+  exitTroubleshootLink: boolean;
+  @Input()
+  closeButton: boolean;
+  constructor() {}
+  ngOnInit(): void {}
 }
