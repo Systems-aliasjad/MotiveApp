5b92e3a7238473d10c6bee1e3209a20f5a487df3
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-08-26 15:32:43 +0500

    fix: info messages

--- Frontend/src/app/message-builder/message-builder.component.ts
+++ Frontend/src/app/message-builder/message-builder.component.ts
@@ -8,16 +8,28 @@ import { ApplicableCodes } from '../shared/constants/constants';
   styleUrls: ['./message-builder.component.scss'],
 })
 export class MessageBuilderComponent implements OnInit {
+  warningImgSrc: string = 'assets/images/super-icons/icon_supericon_all_warning_warning_consumer_regular.svg';
+  successImgSrc: string = 'assets/images/super-icons/icon_supericon_consumer_success_success_consumer_regular.svg';
   openServiceRequestData = {
     reqNo: '436529873',
     reqType: 'Xxxxx xxxxx xxxx',
     dateVisit: 'Jul 10 2019, 10:30 AM',
     status: 'Xxxxx xxxxx xxxx',
   };
-  Section1Data = {
+  openServiceRequestCase1 = {
     header: 'MESSAGES.MOVING_ELIFE_CONNECTION',
     paragraphs: ['MESSAGES.MOVING_ELIFE_CONNECTION_BODY_1', 'MESSAGES.MOVING_ELIFE_CONNECTION_BODY_2'],
   };
+  openServiceRequestCase2 = {
+    header: 'MESSAGES.ACCOUNT_TEMPORARILY_DISCONNECTED',
+    paragraphs: ['MESSAGES.ACCOUNT_TEMPORARILY_DISCONNECTED_BODY'],
+  };
+  openServiceRequestCase3 = {
+    header: 'MESSAGES.ELIFE_CANCELLATION_REQUEST_PROGRESS',
+    paragraphs: ['MESSAGES.ELIFE_CANCELLATION_REQUEST_PROGRESS_BODY'],
+  };
+  imgSrc: string = this.warningImgSrc;
+  Section1Data = this.openServiceRequestCase3;
   openServiceRequestTemplate;
   showLoader: boolean = false;
   constructor() {
