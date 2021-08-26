ed4f53f82bede8616338f5fc4cba2daaabf477a0
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-08-26 18:32:56 +0500

    fix: header config

--- Frontend/src/app/shared/components/header/header.component.ts
+++ Frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { IPageHeader } from '../../constants/types';
 import { SharedService } from '../../shared.service';
 
@@ -7,11 +7,18 @@ import { SharedService } from '../../shared.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
   headerConfig: IPageHeader;
 
   constructor(private sharedService: SharedService) {
     this.headerConfig = this.sharedService.getHeaderConfig();
   }
-  async ngOnInit() {}
+  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log('====================================');
+    // console.log('asdasd', changes);
+    // console.log('====================================');
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+  }
 }
