688578cf904280a51ded220cd3b4c16c7ccb3631
osamasajid36 <osama.sajid@systemsltd.com>
2021-08-27 16:56:38 +0500

    feat: screen H&S 1.3.2

---
+++ Frontend/src/app/shared/constants/customergenreconstants.spec.ts
@@ -0,0 +1,7 @@
+import { CustomerJourneyConstants } from './CustomerJourneyConstants';
+
+describe('CustomerJourneyConstants', () => {
+  it('should create an instance', () => {
+    expect(new CustomerJourneyConstants()).toBeTruthy();
+  });
+});
