974ea3aa1987ee89b876024ff04310e364afa1c6
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-08-30 16:11:53 +0500

    fix: header fix

--- Frontend/src/app/shared/components/router-restart/router-restart.component.ts
+++ Frontend/src/app/shared/components/router-restart/router-restart.component.ts
@@ -11,7 +11,9 @@ export class RouterRestartComponent implements OnInit {
   selectedLang: string;
   instructionList: string[] = ['Unplug the router', 'Wait for 30 seconds', 'Plug the router back in', 'Wait for 5 mins', 'Try to use the internet again'];
 
-  constructor(private sharedService: SharedService) {}
+  constructor(private sharedService: SharedService) {
+    this.sharedService.setHeaderConfig('HEADER.TERMS_AND_CONDITIONS', false);
+  }
 
   ngOnInit() {
     this.selectedLang = this.sharedService.getDefaultLanguage();
