b4bf5685709aa29685eeeaa251c3ccc1f9b145b5
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-08-30 14:56:54 +0500

    fix: type added

--- Frontend/src/app/shared/components/issue-tabs/issue-tabs.component.ts
+++ Frontend/src/app/shared/components/issue-tabs/issue-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ITabTile } from '../../constants/types';
 
 @Component({
   selector: 'app-issue-tabs',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-tabs.component.scss'],
 })
 export class IssueTabsComponent implements OnInit {
-
-  constructor() { }
+  @Input()
+  tabTiles: ITabTile[];
+  constructor() {}
 
   ngOnInit() {}
-
 }
