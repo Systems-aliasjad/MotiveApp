b4f66eef1fb5f361ed938238b11f72026764d00b
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-09-01 10:37:50 +0500

    fix: book a complaint

--- Frontend/src/app/shared/components/book-complaint/book-complaint.component.ts
+++ Frontend/src/app/shared/components/book-complaint/book-complaint.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SharedService } from '../../shared.service';
 import { regExps, errorMessages } from '../../validators/validations';
 
 @Component({
@@ -12,7 +13,8 @@ export class BookComplaintComponent implements OnInit {
   public formGroup: FormGroup;
   error = errorMessages;
 
-  constructor(private formBuilder: FormBuilder, public router: Router) {
+  constructor(private sharedService: SharedService, private formBuilder: FormBuilder, public router: Router) {
+    this.sharedService.setHeaderConfig('HEADER.BOOK_COMPLAINT', false);
     this.formGroup = this.formBuilder.group({
       MobileNo: ['', [Validators.required, Validators.pattern(regExps.phoneNumber)]],
       AlternateContactNo: ['', [Validators.required, Validators.pattern(regExps.phoneNumber)]],
