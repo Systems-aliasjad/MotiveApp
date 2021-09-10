28694eaad0c394145caf440aa4407a7820a4cd9c
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-09-10 15:41:34 +0500

    fix: subscription leak issue

--- Frontend/src/app/landing/landing.component.ts
+++ Frontend/src/app/landing/landing.component.ts
@@ -1,28 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ICard } from '../shared/constants/types';
 import { SharedService } from '../shared/shared.service';
 import { motiveSubscriptions } from '../shared/constants/constants';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, OnDestroy {
   codeType: string;
   selectedLang: string;
   landingPageCards: ICard[];
   showLoader: boolean = false;
+  paramsSubscription: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute, private sharedService: SharedService, public router: Router) {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.activatedRoute.params.subscribe((params: Params) => {
       this.initialization(params);
     });
   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
+  }
 
   initialization(params: Params) {
     this.sharedService.setDefaultValues();
