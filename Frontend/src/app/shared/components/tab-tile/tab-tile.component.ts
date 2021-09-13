17c1d18ebfcf56912b2212389039cf8c5f07f620
SYSTEMS\shoaib.karamat <shoaib.karamat@systemsltd.com>
2021-09-13 18:35:48 +0500

    tiles tab with slider, used in service detail and issue componenet

--- Frontend/src/app/shared/components/tab-tile/tab-tile.component.ts
+++ Frontend/src/app/shared/components/tab-tile/tab-tile.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedService } from '../../shared.service';
 
 @Component({
   selector: 'app-tab-tile',
@@ -6,14 +8,67 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./tab-tile.component.scss'],
 })
 export class TabTileComponent implements OnInit {
-  @Input()
-  imageSrc: string = '';
-  @Input()
-  case: string = ''; // Success-green | Error-red | Default-grey
-  @Input()
-  title: string = '';
+  @Input() tilesList: TabTiles[] = [
+    {
+      className: 'default-state',
+      imgPath: '/assets/images/network-map-icons/icon_smartphone_default.svg',
+      altText: 'Smart Phone Default State',
+      text1: 'Unamed',
+      text2: '21212',
+    },
+    {
+      className: 'okay-state',
+      imgPath: '/assets/images/network-map-icons/icon_smartphone_all_okay.svg',
+      altText: 'Smart Phone okay State',
+      text1: 'Unamed',
+      text2: '21212',
+    },
+    {
+      className: 'error-state',
+      imgPath: '/assets/images/network-map-icons/icon_smartphone_error.svg',
+      altText: 'Smart Phone Error State',
+      text1: 'Unamed',
+      text2: '21212',
+    },
+    {
+      className: 'pending-state',
+      imgPath: '/assets/images/network-map-icons/icon_smartphone_action_pending.svg',
+      altText: 'Smart Phone Pending State',
+      text1: 'Unamed',
+      text2: '21212',
+    },
+    {
+      className: 'pending-state',
+      imgPath: '/assets/images/network-map-icons/icon_smartphone_action_pending.svg',
+      altText: 'Smart Phone Pending State',
+      text1: 'Unamed',
+      text2: '21212',
+    },
+  ];
 
-  constructor() {}
+  slideOpts = {
+    slidesPerView: 4,
+    spaceBetween: 1,
+    loop: false,
+    grid: {
+      row: 2,
+    },
+  };
+  constructor(private sharedService: SharedService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initialization();
+  }
+
+  initialization() {
+    this.sharedService.setDefaultValues();
+  }
+}
+
+export class TabTiles {
+  className: string;
+  imgPath: string;
+  altText: string;
+  text1: string;
+  text2: string;
 }
