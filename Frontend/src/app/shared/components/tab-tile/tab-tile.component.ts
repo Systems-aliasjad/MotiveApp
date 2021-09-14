df94d68673c56a966bcb471a44b952488fae1f56
SYSTEMS\muhammad.hameed <muhammad.hameed@systemsltd.com>
2021-09-14 12:51:03 +0500

    UI Fixes

--- Frontend/src/app/shared/components/tab-tile/tab-tile.component.ts
+++ Frontend/src/app/shared/components/tab-tile/tab-tile.component.ts
@@ -48,11 +48,12 @@ export class TabTileComponent implements OnInit {
 
   slideOpts = {
     slidesPerView: 4,
-    /* spaceBetween: 1, */
+    /* spaceBetween: 100, */
     loop: false,
     grid: {
       row: 2,
     },
+    
   };
   constructor(private sharedService: SharedService, private router: Router) {}
 
