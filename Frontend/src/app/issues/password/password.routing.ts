19cd930cdcc2d9b1dd8ea8f27e1a355da725b14d
SYSTEMS\shoaib.karamat <shoaib.karamat@systemsltd.com>
2021-09-16 16:48:50 +0500

    footer-btns-container

--- Frontend/src/app/issues/password/password.routing.ts
+++ Frontend/src/app/issues/password/password.routing.ts
@@ -3,6 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { InternetPasswordResetComponent } from './routes/internet-password-reset.component';
 
 import { MainComponent } from './routes/main.component';
+import { RequestFailComponent } from './routes/request-fail.component';
+import { ResetPasswordFailComponenet } from './routes/reset-password-fail.component';
+import { ResetRouterWiFiPasswordSuccess } from './routes/reset-router-wifi-password-success.component';
+import { ResetInternetPasswordSuccessDetailComponent } from './routes/rest-internet-password-success-detail.component';
+import { TryAgainErrorComponent } from './routes/try-again-error.component';
+import { UnableToReachRouterFailComponent } from './routes/unable-reach-router-fail.component';
+import { UnableToReachRouterComponent } from './routes/unable-reach-router.component';
 
 const routes: Routes = [
   {
@@ -14,6 +21,34 @@ const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+  },
+  {
+    path: 'unable-to-process-request',
+    component: RequestFailComponent,
+  },
+  {
+    path: 'unable-to-reset-password',
+    component: ResetPasswordFailComponenet,
+  },
+  {
+    path: 'unable-to-reach-router',
+    component: UnableToReachRouterComponent,
+  },
+  {
+    path: 'unable-to-reach-router-failed',
+    component: UnableToReachRouterFailComponent,
+  },
+  {
+    path: 'reset-router-password-success',
+    component: ResetRouterWiFiPasswordSuccess,
+  },
+  {
+    path: 'internet-password-reset-detail',
+    component: ResetInternetPasswordSuccessDetailComponent,
+  },
+  {
+    path: 'internet-password-reset-fail',
+    component: TryAgainErrorComponent,
   },
 ];
 
