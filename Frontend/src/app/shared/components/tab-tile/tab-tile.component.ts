ab046fc3585dedf5de9f351ca8e10df047a1e0eb
Shahan Fareed <shahan.fareed@systemsltd.com>
2021-09-28 12:19:43 +0500

    added arrows on slider with responsive slides

--- Frontend/src/app/shared/components/tab-tile/tab-tile.component.ts
+++ Frontend/src/app/shared/components/tab-tile/tab-tile.component.ts
@@ -54,6 +54,23 @@ export class TabTileComponent implements OnInit {
     grid: {
       row: 2,
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
   };
   constructor(private sharedService: SharedService, private router: Router) {}
 
