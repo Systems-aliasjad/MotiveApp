2a5de5368bd07162498403339a1ac12921a6acd3
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-09-28 12:51:48 +0500

    doc: comments added

--- Frontend/src/app/http-interceptor/auth-interceptor.ts
+++ Frontend/src/app/http-interceptor/auth-interceptor.ts
@@ -9,10 +9,21 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // Get the auth token from the service.
     const baseUrl = environment.API_BASE_URL;
-    if (req.url.includes('MOTIVE_APP_SELFCARE/token')) {
+    /**
+     * Ingores the request for the static angular file - Images
+     */
+    if (req.url.includes('assets')) {
+      return next.handle(req);
+    } else if (req.url.includes('MOTIVE_APP_SELFCARE/token')) {
+      /**
+       *  For First API call when Motive App loads form the CIT
+       */
       const authReq = req.clone({ url: `${baseUrl}${req.url}` });
       return next.handle(authReq);
     } else {
+      /**
+       * All other API Calls should execute the following logic and add common auth token
+       */
       const authToken: string = this.auth.getAuthorizationToken();
       const authReq = req.clone({ url: `${baseUrl}${req.url}`, setHeaders: { Authorization: `Bearer ${authToken}` } });
       return next.handle(authReq);
