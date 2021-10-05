7a44d8b323363f87ffd54a0a74b62eca2788d7b5
SYSTEMS\shoaib.karamat <shoaib.karamat@systemsltd.com>
2021-10-05 12:37:39 +0500

    transfer package component is moved to shared component links updated

--- Frontend/src/app/shared/shared.module.ts
+++ Frontend/src/app/shared/shared.module.ts
@@ -30,6 +30,7 @@ import { GenericErrorComponent } from './components/generic-error/generic-error.
 import { ThreeLayersComponent } from './components/network-diagrams/three-layers/three-layers.component';
 import { HeaderSingleLineComponent } from './components/header-single-line/header-single-line.component';
 import { HeaderMultiLineComponent } from './components/header-multi-line/header-multi-line.component';
+import { TransferPackageComponent } from './components/transfer-package/transfer-package.component';
 
 @NgModule({
   declarations: [
@@ -56,6 +57,7 @@ import { HeaderMultiLineComponent } from './components/header-multi-line/header-
     MotiveMessageFeedbackComponent,
     HeaderSingleLineComponent,
     HeaderMultiLineComponent,
+    TransferPackageComponent,
   ],
   imports: [
     CommonModule,
@@ -89,6 +91,7 @@ import { HeaderMultiLineComponent } from './components/header-multi-line/header-
     MotiveMessageFeedbackComponent,
     HeaderMultiLineComponent,
     HeaderSingleLineComponent,
+    TransferPackageComponent,
   ],
 
   entryComponents: [],
