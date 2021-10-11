898074e284a75ca1eca735bf9952d91a44ad1bda
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-10-11 17:53:34 +0500

    fix: seven-layer component fix

--- Frontend/src/app/shared/components/network-diagrams/seven-layer/seven-layer.component.ts
+++ Frontend/src/app/shared/components/network-diagrams/seven-layer/seven-layer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-seven-layer',
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seven-layer.component.scss'],
 })
 export class SevenLayerComponent implements OnInit {
-
-  constructor() { }
+  @Input()
+  devices;
+  constructor() {}
 
   ngOnInit() {}
-
 }
