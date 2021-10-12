f6e5ce2da4917a1f584514ee2d04e939f2418a32
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-10-12 16:48:55 +0500

    fix: interceptor

--- Frontend/src/app/http-interceptor/auth-interceptor.ts
+++ Frontend/src/app/http-interceptor/auth-interceptor.ts
@@ -1,32 +1,43 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 import { environment } from 'src/environments/environment';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get the auth token from the service.
+    let myReq;
     const baseUrl = environment.API_BASE_URL;
     /**
      * Ingores the request for the static angular file - Images
      */
     if (req.url.includes('assets')) {
-      return next.handle(req);
+      myReq = req;
     } else if (req.url.includes('token')) {
       /**
        *  For First API call when Motive App loads form the CIT
        */
-      const authReq = req.clone({ url: `${baseUrl}${req.url}` });
-      return next.handle(authReq);
+      myReq = req.clone({ url: `${baseUrl}${req.url}` });
     } else {
       /**
        * All other API Calls should execute the following logic and add common auth token
        */
       const authToken: string = this.auth.getAuthorizationToken();
-      const authReq = req.clone({ url: `${baseUrl}${req.url}`, setHeaders: { Authorization: `Bearer ${authToken}` } });
-      return next.handle(authReq);
+      myReq = req.clone({ url: `${baseUrl}${req.url}`, setHeaders: { Authorization: `Bearer ${authToken}` } });
     }
+
+    return next.handle(myReq).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('Response event--->>>', event.status);
+        }
+        return event;
+      })
+    );
   }
 }
