20cafa1f8cb81e473ea5c9dc872d568c96940636
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-10-20 16:52:28 +0500

    fix: missing file

---
+++ Frontend/src/app/issues/internet/routes/troubleshoot-complete-message.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { successImgSrc } from 'src/app/shared/constants/constants';
+import { IMotiveButton } from 'src/app/shared/constants/types';
+import { CustomerJourneyConstants } from 'src/app/shared/constants/CustomerJourneyConstants';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/shared/shared.service';
+import { BackendService } from 'src/app/services/backend.service';
+
+@Component({
+  selector: 'internet-troubleshoot-complete-message',
+  template: `<motive-message
+    [imgSrc]="imgSrc"
+    [Section1Data]="Section1Data"
+    [Section2Data]="Section2Data"
+    [Section2Template]="Section2Template"
+    [button1]="button1"
+    (button1Click)="button1Listener()"
+    [button2]="button2"
+    (button2Click)="button2Listener()"
+  ></motive-message>`,
+})
+export class InternetTroubleshootCompleteMessageComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  Section1Data;
+  Section2Template;
+  Section2Data;
+  imgSrc;
+  button1: IMotiveButton = {
+    title: 'BUTTONS.REQUEST_PAID_TECHNICIAN_VISIT',
+    type: 'primary',
+  };
+  button2: IMotiveButton = {
+    title: 'BUTTONS.CANCEL',
+    type: 'link',
+  };
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private sharedService: SharedService, private backendService: BackendService) {}
+
+  ngOnInit() {
+    this.subscription = this.activatedRoute.data.subscribe(() => {
+      this.updateHeader();
+    });
+    this.updatePageContent();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  updateHeader() {}
+
+  updatePageContent() {
+    this.Section1Data = CustomerJourneyConstants.troubleshootComplete;
+    this.imgSrc = successImgSrc;
+  }
+
+  button1Listener() {
+    this.router.navigate(['issues/internet/book-complaint']);
+  }
+
+  button2Listener() {
+    this.sharedService.setLoader(true);
+    this.backendService.bookComplaint({ mobileNo: localStorage.getItem('CUS_MOBILE_NO'), remarks: '', ci7: true }).subscribe(() => {
+      this.sharedService.setLoader(false);
+      this.router.navigate(['/thanks']);
+    });
+  }
+}
