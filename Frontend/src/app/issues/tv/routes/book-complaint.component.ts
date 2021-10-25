435d8af9112a0f3b8783fe09617d98ef7bbaa312
osamasajid36 <osama.sajid@systemsltd.com>
2021-10-25 11:26:56 +0500

    tv module book complaint and raised screens

---
+++ Frontend/src/app/issues/tv/routes/book-complaint.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IMotiveButton, IPageHeader } from 'src/app/shared/constants/types';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/shared/shared.service';
+import { Location } from '@angular/common';
+import { FormGroup } from '@angular/forms';
+import { BackendService } from 'src/app/services/backend.service';
+
+@Component({
+  selector: 'book-complaint',
+  template: `<app-book-complaint
+    [headerConfig]="headerConfig"
+    [button1]="button1"
+    (button1Click)="button1Listener($event)"
+    [button2]="button2"
+    (button2Click)="button2Listener()"
+  ></app-book-complaint>`,
+})
+export class BookComplaintComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  button1: IMotiveButton = {
+    type: 'primary',
+    title: 'BUTTONS.BOOK_AN_APPOINTMENT',
+    explanatoryNote: '',
+  };
+
+  button2: IMotiveButton = {
+    type: 'link',
+    title: 'BUTTONS.BACK',
+    explanatoryNote: '',
+  };
+
+  formGroup: FormGroup;
+  constructor(
+    private backendService: BackendService,
+    private sharedService: SharedService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.activatedRoute.data.subscribe(() => {
+      this.updateHeader();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  updateHeader() {
+    //this.sharedService.setHeaderConfig('HEADER.BOOK_A_COMPLAINT', false);
+  }
+
+  headerConfig: IPageHeader = {
+    pageTitle: 'HEADER.BOOK_A_COMPLAINT',
+    showBackBtn: true,
+  };
+
+  button1Listener(_event) {
+    this.formGroup = _event;
+    this.sharedService.setLoader(true);
+    this.backendService.bookComplaint({ ...this.formGroup.value, ci7: true }).subscribe((data: any) => {
+      this.sharedService.setLoader(false);
+      this.sharedService.setApiResponseData({ ...data?.result?.responseData, status: 'open' });
+      this.router.navigate(['/issues/tv/complaint-raised-successfull']);
+    });
+  }
+
+  button2Listener() {
+    this.location.back();
+  }
+}
---
+++ Frontend/src/app/issues/tv/routes/complaint-raised-successfully-message.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ApplicableCodes, successImgSrc, warningImgSrc } from 'src/app/shared/constants/constants';
+import { IMotiveButton } from 'src/app/shared/constants/types';
+import { CustomerJourneyConstants } from 'src/app/shared/constants/CustomerJourneyConstants';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/shared/shared.service';
+
+/**
+ * Open Service Request present
+ */
+@Component({
+  selector: 'app-tv-complaint-raised-successfully-message',
+  template: `<motive-message
+    [imgSrc]="imgSrc"
+    [Section1Data]="Section1Data"
+    [Section2Data]="Section2Data"
+    [Section2Template]="Section2Template"
+    [button1]="button1"
+    (button1Click)="button1Listener()"
+    [button2]="button2"
+    (button2Click)="button2Listener()"
+  ></motive-message>`,
+})
+export class ComplaintRaisedSuccessfullyMessageComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  Section1Data;
+  Section2Template;
+  Section2Data;
+  imgSrc;
+
+  button1: IMotiveButton = {
+    type: 'link',
+    title: 'BUTTONS.TRACK_COMPLAINT_STATUS',
+    explanatoryNote: '',
+  };
+
+  button2: IMotiveButton = {
+    type: 'secondary',
+    title: 'BUTTONS.DONE',
+  };
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private sharedService: SharedService) {}
+
+  ngOnInit() {
+    this.subscription = this.activatedRoute.data.subscribe(() => {
+      this.updateHeader();
+    });
+    this.updatePageContent();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  updateHeader() {
+    this.sharedService.setHeaderConfig('', false, false);
+  }
+
+  updatePageContent() {
+    this.imgSrc = successImgSrc;
+    this.Section1Data = CustomerJourneyConstants.InstallNewRouterComplaintRaisedSuccessfully;
+    this.Section2Template = ApplicableCodes.installNewRouterComplaintBookSuccessfullyTemplate;
+    const temp = this.sharedService.getApiResponseData();
+    this.Section2Data = {
+      referenceNo: temp?.referenceNo ?? '-',
+      dateOfVisit: temp?.dateOfVisit ?? '-',
+      status: temp?.status ?? '-',
+    };
+  }
+
+  button1Listener() {}
+  button2Listener() {
+    this.router.navigate(['/thanks']);
+  }
+}
